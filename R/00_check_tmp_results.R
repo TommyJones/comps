9d7e36bff0a1cbfbfc35d7317a100a8c79e859e3
Tommy Jones <jones.thos.w@gmail.com>
2020-04-07 20:04:22 -0400

    Get through 1st part of LDA portion

--- R/00_check_tmp_results.R
+++ R/00_check_tmp_results.R
@@ -1,27 +1,9 @@
-
 library(tidyverse)
-
-library(tidytext)
 
 library(textmineR)
 
 library(coda)
 
-load("data_derived/20_newsgroups_formatted.RData")
-
-# random search over a range of k
-# alternatively, use sigopt for search over k
-# store coherence and likelihood
-
-k_range <- sample(5:2000, 100)
-
-# create a directory to store models for future reference if needed
-if (! file.exists("data_derived/lda_choose_k")) {
-  dir.create("data_derived/lda_choose_k")
-}
-
-
-# function to evaluate outputs of model
 calc_eval <- function(m, dtm_train, dtm_test) {
   
   # check convergence
@@ -53,7 +35,7 @@ calc_eval <- function(m, dtm_train, dtm_test) {
     dtm = dtm_train,
     phi = m$phi,
     theta = m$theta,
-    cpus = 2
+    cpus = 1
   )
   
   # in sample perplexity
@@ -86,7 +68,7 @@ calc_eval <- function(m, dtm_train, dtm_test) {
     dtm = dtm_test[, vocab_intersect],
     phi = phi_adjusted,
     theta = theta_hat,
-    cpus = 2
+    cpus = 1
   )
   
   # out-of-sample perplexity
@@ -107,64 +89,60 @@ calc_eval <- function(m, dtm_train, dtm_test) {
   eval
 }
 
-model_eval <- parallel::mclapply(
-  k_range,
-  function(k){
-    
-    # randomly sample 1,000 docs to train on 
-    train_rows <- sample(1:nrow(dtm), 1000)
-    
-    dtm_lda1 <- dtm[train_rows, ]
-    
-    dtm_lda2 <- dtm[-train_rows, ]
-    
-    # need smaller vocabulary set to calculate likelihood without 
-    # overflow/underflow error
-    dtm_lda1 <- dtm_lda1[, colSums(dtm_lda1) > 0]
-    
-    dtm_lda2 <- dtm_lda2[, colSums(dtm_lda2) > 0]
-    
-    alpha <- 0.01
-    
-    beta <- colSums(dtm_lda1) / sum(dtm_lda1) * 135
-    
-    m <- FitLdaModel(
-      dtm = dtm_lda1,
-      k = k,
-      iterations = 200,
-      burnin = 175,
-      alpha = alpha,
-      beta = beta,
-      optimize_alpha = FALSE,
-      calc_likelihood = TRUE,
-      calc_coherence = TRUE,
-      calc_r2 = FALSE,
-      cpus = 2
-      )
-    
 
-    # make a one-row eval tibble
-    eval <- calc_eval(
-      m = m,
-      dtm_train = dtm_lda1,
-      dtm_test = dtm_lda2
-    )
-    
-    # save model
-    filename <- paste0(
-      "data_derived/lda_choose_k/lda_k_",
-      k,
-      ".RData")
-    
-    save(eval, m, train_rows, file = filename)
-    
-    # return eval tibble
-    return(eval)
-    
-  }, mc.cores = parallel::detectCores() - 1)
+files <- list.files("data_derived/lda_choose_k", full.names = T)
 
-# combine the tibble eval
+files <- setdiff(files, "data_derived/lda_choose_k/archive")
 
-eval_lda <- do.call(rbind, model_eval)
+# load("data_derived/20_newsgroups_formatted.RData")
+# 
+# eval_lda <- parallel::mclapply(
+#   files, 
+#   function(f) {
+#     
+#     load(f)
+#     
+#     eval <- calc_eval(
+#       m = m,
+#       dtm_train = dtm[train_rows, ],
+#       dtm_test = dtm[-train_rows, ]
+#     )
+#     
+#     eval
+#     
+#   },
+#   mc.cores = parallel::detectCores() - 1)
 
-save(eval_lda, file = "data_derived/eval_lda_choose_k.RData")
+load_eval <- function(f){
+  load(f)
+  
+  eval
+}
+
+
+eval_lda <- lapply(
+  files,
+  load_eval
+)
+
+eval_lda <- do.call(rbind, eval_lda)
+
+# files2 <- list.files("data_derived/lda_choose_k", full.names = T)
+# 
+# files2 <- setdiff(files2, c("data_derived/lda_choose_k/archive", files))
+# 
+# eval_lda <- rbind(
+#   eval_lda,
+#   do.call(
+#     rbind,
+#     lapply(files2, load_eval)
+#   )
+# )
+# 
+# files <- c(files, files2)
+
+ggplot(eval_lda, mapping = aes(x = k, y = coherence_in)) + 
+  geom_point(mapping = aes(color = converged)) + 
+  geom_smooth() + 
+  geom_point(mapping = aes(x = k, y = coherence_out)) +
+  geom_smooth(mapping = aes(x = k, y = coherence_out))
