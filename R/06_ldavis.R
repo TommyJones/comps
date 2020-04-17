a7fd1b4071fb0313d8ec4b32759901601ad60cf2
Tommy Jones <jones.thos.w@gmail.com>
2020-04-16 23:00:40 -0400

    Different intertopic distance visualizations

---
+++ R/06_ldavis.R
@@ -0,0 +1,40 @@
+# run LDAvis for better depth
+
+library(textmineR)
+
+library(LDAvis)
+
+load("data_derived/lda_retrain.RData")
+
+load("data_derived/20_newsgroups_formatted.RData")
+
+# create json files for each model
+
+models <- list(
+  result[[1]],
+  result[[2]]$models[[2]],
+  result[[3]]$models[[5]]
+)
+
+json <- parallel::mclapply(
+  models,
+  function(m) {
+    createJSON(
+      phi = m$phi, 
+      theta = m$theta, 
+      doc.length = rowSums(dtm_bigram[lda_rows, ]), 
+      vocab = colnames(m$phi), 
+      term.frequency = colSums(dtm_bigram[, colnames(m$phi)])
+    )
+  },
+  mc.cores = 3
+)
+
+save(
+  json,
+  file = "data_derived/ldavis_json.RData"
+)
+
+# serVis(json[[1]], out.dir = 'vis', 
+#        open.browser = TRUE)
+
